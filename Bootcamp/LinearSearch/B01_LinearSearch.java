eed5ad7e10a91795e15f16e7fdc2d522dd09756d
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-03-02 22:32:27 +0530

    Update B01_LinearSearch.java

--- Bootcamp/LinearSearch/B01_LinearSearch.java
+++ Bootcamp/LinearSearch/B01_LinearSearch.java
@@ -1,5 +1,6 @@
 package Bootcamp.LinearSearch;
-
+/* a linear search or sequential search is a method for finding an element within a list. 
+It sequentially checks each element of the list until a match is found or the whole list has been searched.*/
 public class B01_LinearSearch {
     public static void main(String[] args) {
         int [] nums = {12,34,56,78,21,54,3,76,89};
