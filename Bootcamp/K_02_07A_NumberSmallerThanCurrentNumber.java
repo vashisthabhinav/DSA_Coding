d546bb2c0d053f319fa7450521aeecf7ee8a8147
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-03-06 21:31:54 +0530

    Update K_02_07A_NumberSmallerThanCurrentNumber.java

--- Bootcamp/K_02_07A_NumberSmallerThanCurrentNumber.java
+++ Bootcamp/K_02_07A_NumberSmallerThanCurrentNumber.java
@@ -3,11 +3,13 @@ package Bootcamp;
 import java.util.Arrays;
 
 public class K_02_07A_NumberSmallerThanCurrentNumber {
+    
     /*
     Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
     That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
     Return the answer in an array.
      */
+    
     public static int[] smallerNumbersThanCurrent(int[] nums) {
         int [] result =  new int[nums.length];
 
