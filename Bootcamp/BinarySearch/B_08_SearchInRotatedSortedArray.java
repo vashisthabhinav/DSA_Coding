92651cf0cde81b43b9b47307d11e2b2fdf9208d2
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-04-27 06:20:25 +0530

    Add files via upload

---
+++ Bootcamp/BinarySearch/B_08_SearchInRotatedSortedArray.java
@@ -0,0 +1,20 @@
+package Bootcamp.BinarySearch;
+
+public class B_08_SearchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        /*
+        In 4,5,6,7,0,1,2
+        It is ascending, from 4-7 & 0-2, and descending only from 7-0
+        7 is the pivot.
+        i.e., pivot is when mid>mid+1 or mid<mid-1
+         */
+        int index = -1;
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {4,5,6,7,0,1,2};
+        int target  = 3;
+        System.out.println(search(arr,target));
+    }
+}
