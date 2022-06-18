c06d0436cc8b4054d772d8bd6ee4b25ce85edc17
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-06-18 23:16:32 +0530

    Adding files to the repository.

--- Bootcamp/BinarySearch/B_17_TwoSum2.java
+++ Bootcamp/BinarySearch/B_17_TwoSum2.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class B_17_TwoSum2 {
     public static void main(String[] args) {
-        int [] numbers = {2,3,4};
-        int target = 6;
+        int [] numbers = {2,7,11,15};
+        int target = 26;
         System.out.println(Arrays.toString(twoSum(numbers,target)));
         System.out.println(Arrays.toString(twoSum1(numbers,target)));
+        System.out.println(Arrays.toString(twoSum2(numbers,target)));
     }
     public static int[] twoSum(int[] numbers, int target) {
         //Q167
@@ -19,7 +20,7 @@ public class B_17_TwoSum2 {
                 }
             }
         }
-        return new int[]{};
+        return new int[]{-1,-1};
     }
     public static int[] twoSum1(int[] numbers, int target) {
         //Q167
@@ -43,5 +44,22 @@ public class B_17_TwoSum2 {
             }
         }
         return new int[]{-1,-1};
+    }
+    public static int[] twoSum2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int low = i + 1;
+            int high = nums.length - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (nums[mid] == target - nums[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (nums[mid] > target - nums[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
